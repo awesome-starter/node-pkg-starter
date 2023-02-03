9b7b3296ad4913ece698c2e89308d079402bf081
chengpeiquan <chengpeiquan@chengpeiquan.com>
2023-02-03 23:51:09 +0800

    chore: add deps

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
+const { resolve } = require('path')
+const { isEmpty } = require('@bassist/node-utils')
+const { randomString } = require('@bassist/utils')
 const confirmExit = require('./libs/confirmExit')
 
-function start() {
+async function run() {
+  // In pkg, you must use `cwd` instead `__dirname`
+  const cwd = process.cwd()
+
+  // Use some wrapped Node methods
+  // https://paka.dev/npm/@bassist/node-utils
+  console.log('Check the current directory is empty:')
+  console.log(isEmpty(resolve(cwd)))
+  console.log()
+
+  // Use some commonly used utility functions
+  // https://paka.dev/npm/@bassist/utils
+  console.log('Get a random string:')
+  console.log(randomString())
+  console.log()
+
   confirmExit('Hello World!')
 }
-start()
+run().catch((e) => {
+  console.log(e)
+})
